6156cf53e8a7f47bb7b89a8d56090b82cc39dc77
YM <YM@PC-2004>
2026-08-15 12:31:34 +0300

    Hata ve iyileştirmeler, ekran görüntüleri.

--- IdentityMail.Web/DTOs/UserDtos/LoginDto.cs
+++ IdentityMail.Web/DTOs/UserDtos/LoginDto.cs
@@ -1,8 +1,16 @@
-﻿namespace IdentityMail.Web.DTOs.UserDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityMail.Web.DTOs.UserDtos
 {
     public class LoginDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [Required(ErrorMessage = "E-posta adresi zorunludur.")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi giriniz.")]
+        public string Email { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage = "Şifre zorunludur.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
     }
 }
